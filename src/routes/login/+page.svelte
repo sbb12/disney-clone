55ec44b9ec5b6c2c724aea6d557d6d182f894366
Sercan Sokmen <sercansokmen0@gmail.com>
2023-05-18 21:16:36 +0100

    login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<bg class="bg-[#040714] fixed h-screen top-0 transition-opacity w-full z-[-3]"></bg>
+
+<nav class="flex w-full pl-8 align-middle border-b border-gray-600 h-14 sm:h-20">
+    <img src="/images/logo.svg" alt="disney logo" class="w-16 md:w-20">
+</nav>
+
+<main class="top-20 flex flex-col mx-auto text-gray-50 text-center max-w-[400px] min-h-[calc(100vh-300px)] py-[5vw] px-4">
+    <h3 class="text-4xl font-semibold">No registration</h3>
+    <p class="py-3">User features are beyond the current scope of this project and will be added later. Simply just click continue to view the rest of the site as if you were logged in.</p>
+    <a href="/home">
+        <button class="bg-[#0063e5] text-white px-4 py-2 rounded-md mt-4">Continue</button>
+    </a>
+</main>
